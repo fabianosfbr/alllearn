3fb8156f3f5485f6b15c3d9db05866a0cd48a7bb
caioLMSE <caio100xa@gmail.com>
2022-10-21 14:21:01 -0300

    vídeo do professor incluso na home

--- resources/views/web/default/pages/home.blade.php
+++ resources/views/web/default/pages/home.blade.php
@@ -180,7 +180,7 @@
                     </div>
                     <div class="col-sm mt-30 text-uppercase font-weight-bold">
                         <p>Vídeo de apresentação para Professores</p>
-                      <iframe width="100%" height="300" src="https://www.youtube.com/embed/xSUgYW_ySIw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                      <iframe width="100%" height="300" src="https://www.youtube.com/embed/meQXoltxZXQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                   </div>
             </div> 
