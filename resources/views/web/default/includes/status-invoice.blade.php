ac36c8bc4f3944ad3117d405b3329009dd3bc1d6
CaioLMSE <tecnologia@leandromorilhas.com.br>
2023-03-20 10:22:35 -0300

    ajustes no tamanho do span do status

--- resources/views/web/default/includes/status-invoice.blade.php
+++ resources/views/web/default/includes/status-invoice.blade.php
@@ -1,11 +1,11 @@
 <span class="font-12 font-weight-500">
     @if ($status == 'Cancelado' or $status == 'Recusado')
-    <span class="badge badge-danger">{{$status}}</span>
+    <span style="width: 90px; height: 20px;" class="badge badge-danger">{{$status}}</span>
     @elseif ($status == 'Pendente')
-    <span class="badge badge-secondary">{{$status}}</span>
+    <span style="width: 90px; height: 20px;" class="badge badge-secondary">{{$status}}</span>
     @elseif ($status == 'Recebido' or $status == 'Confirmado' or $status == 'Pago')
-    <span class="badge badge-primary">{{$status}}</span>
+    <span style="width: 90px; height: 20px;" class="badge badge-primary">{{$status}}</span>
     @elseif ($status == 'Vencido')
-    <span class="badge badge-warning">{{$status}}</span>
+    <span style="width: 90px; height: 20px;" class="badge badge-warning">{{$status}}</span>
     @endif
 </span>
